f43e7bc0173f87f3cac600e8da356dd0c2d68537
brunoandreotti <brunoandreotti@outlook.com>
2022-08-11 14:58:46 -0300

    ✨ feat: added deleteById method in cards repository

--- src/repositories/cards/ICardsRepository.ts
+++ src/repositories/cards/ICardsRepository.ts
@@ -4,6 +4,7 @@ import { CardsData } from '../../dtos/CardsData.dto'
 export interface ICardsRepository {
   create(data: CardsData): Promise<CardsData>
   countCards(): Promise<number>
+  deleteById(id: number): Promise<CardsData>
   findByName(name: string): Promise<CardsData | null>
   findById(id: number): Promise<CardsData | null>
   findAll(page: number, items: number, name: string): Promise<CardsData[]>
