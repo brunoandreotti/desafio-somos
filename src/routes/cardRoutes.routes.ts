1e069f774320f214997806b350c14cf041b31e07
brunoandreotti <brunoandreotti@outlook.com>
2022-08-10 21:47:48 -0300

    ✨ feat: created update cards by id endpoint

--- src/routes/cardRoutes.routes.ts
+++ src/routes/cardRoutes.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { CreateCardsController } from '../controllers/cards/CreateCardsController'
 import { FindAllCardsController } from '../controllers/cards/FindAllCardsController'
 import { FindCardsByIdController } from '../controllers/cards/FindCardsByIdController'
+import { UpdateCardsByIdController } from '../controllers/cards/UpdateCardsByIdController'
 import { validationMiddleware } from '../middlewares/validationMiddleware'
 import { createCardValidationRules } from '../utils/validation/createCardValidationRules'
 import { idParamValidationRules } from '../utils/validation/idParamValidationRules'
@@ -22,5 +23,7 @@ cardRoutes.get(
   validationMiddleware,
   FindCardsByIdController.handle,
 )
+
+cardRoutes.patch('/:id', UpdateCardsByIdController.handle)
 
 export { cardRoutes }
