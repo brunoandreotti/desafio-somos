fd2af14a555bd3233d7f91125456a1346a91de04
brunoandreotti <brunoandreotti@outlook.com>
2022-08-11 23:59:07 -0300

    ✨ feat: added validation to duel request body

--- src/routes/duelRoutes.routes.ts
+++ src/routes/duelRoutes.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { CreateDuelController } from '../controllers/duels/CreateDuelsController'
+import { validationMiddleware } from '../middlewares/validationMiddleware'
+import { duelBodyValidationRule } from '../utils/validation/duelBodyValidationRules'
 
 const duelRoutes = Router()
 
-duelRoutes.post('/', CreateDuelController.handle)
+duelRoutes.post('/', duelBodyValidationRule(), validationMiddleware, CreateDuelController.handle)
 
 export { duelRoutes }
