37f23a7290198386870d0e102d25bcdc8ae101b9
brunoandreotti <brunoandreotti@outlook.com>
2022-08-11 23:48:08 -0300

    ♻ refactor: created compareCardsfunction.ts file

--- src/services/duels/CreateDuelsService.ts
+++ src/services/duels/CreateDuelsService.ts
@@ -1,9 +1,8 @@
-import { Attributes } from '@prisma/client'
-import { CardsData } from '../../dtos/CardsData.dto'
 import { DuelData } from '../../dtos/DuelData.dto'
 import { ICardsRepository } from '../../repositories/cards/ICardsRepository'
 import { IDuelsRepository } from '../../repositories/duels/IDuelsRepository'
 import { AppError } from '../../utils/AppError'
+import { compareCards } from '../../utils/compareCardsFunction'
 
 class CreateDuelsService {
   constructor(
@@ -25,111 +24,6 @@ class CreateDuelsService {
 
     if (!playerTwoCard) {
       throw new AppError('Carta do jogador dois não encontrada', 404)
-    }
-
-    const compareCards = (
-      playerOneCard: CardsData,
-      playerTwoCard: CardsData,
-    ) => {
-      const result = {
-        winner: -1,
-        loser: -1,
-        details: {
-          hp: -1,
-          attack: -1,
-          defense: -1,
-          specialAttack: -1,
-          specialDefense: -1,
-          speed: -1,
-        },
-      }
-
-      let playerOneWins = 0
-      let playerTwoWins = 0
-
-      const playerOneCardAttributes: Attributes =
-        Object.values(playerOneCard)[2]
-      const playerTwoCardAttributes: Attributes =
-        Object.values(playerTwoCard)[2]
-
-      if (playerOneCardAttributes.hp > playerTwoCardAttributes.hp) {
-        result.details.hp = playerOneCardId
-        playerOneWins++
-      } else if (playerOneCardAttributes.hp < playerTwoCardAttributes.hp) {
-        result.details.hp = playerTwoCardId
-        playerTwoWins++
-      }
-
-      if (playerOneCardAttributes.attack > playerTwoCardAttributes.attack) {
-        result.details.attack = playerOneCardId
-        playerOneWins++
-      } else if (
-        playerOneCardAttributes.attack < playerTwoCardAttributes.attack
-      ) {
-        result.details.attack = playerTwoCardId
-        playerTwoWins++
-      }
-
-      if (playerOneCardAttributes.defense > playerTwoCardAttributes.defense) {
-        result.details.defense = playerOneCardId
-        playerOneWins++
-      } else if (
-        playerOneCardAttributes.defense < playerTwoCardAttributes.defense
-      ) {
-        result.details.defense = playerTwoCardId
-        playerTwoWins++
-      }
-
-      if (
-        playerOneCardAttributes.specialAttack >
-        playerTwoCardAttributes.specialAttack
-      ) {
-        result.details.specialAttack = playerOneCardId
-        playerOneWins++
-      } else if (
-        playerOneCardAttributes.specialAttack <
-        playerTwoCardAttributes.specialAttack
-      ) {
-        result.details.specialAttack = playerTwoCardId
-        playerTwoWins++
-      }
-
-      if (
-        playerOneCardAttributes.specialDefense >
-        playerTwoCardAttributes.specialDefense
-      ) {
-        result.details.specialDefense = playerOneCardId
-        playerOneWins++
-      } else if (
-        playerOneCardAttributes.specialDefense <
-        playerTwoCardAttributes.specialDefense
-      ) {
-        result.details.specialDefense = playerTwoCardId
-        playerTwoWins++
-      }
-
-      if (playerOneCardAttributes.defense > playerTwoCardAttributes.defense) {
-        result.details.defense = playerOneCardId
-        playerOneWins++
-      } else if (
-        playerOneCardAttributes.defense < playerTwoCardAttributes.defense
-      ) {
-        result.details.defense = playerTwoCardId
-        playerTwoWins++
-      }
-
-      if (playerOneWins > playerTwoWins) {
-        result.winner = playerOneCardId
-        result.loser = playerTwoCardId
-      } else if (playerOneWins < playerTwoWins) {
-        result.winner = playerTwoCardId
-        result.loser = playerOneCardId
-      } else {
-        result.winner = -1
-        result.loser = -1
-      }
-
-      return result
     }
 
     const result = compareCards(playerOneCard, playerTwoCard)
